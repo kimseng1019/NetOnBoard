73e79c0f50ff323ae380049af02e35a37b25a678
Lan Chee Hong <chlan@netonboard.com>
2018-01-26 10:23:49 +0800

    calendarActivity added, waiting for API

--- app/src/main/java/com/example/netonboard/netonboardv2/CalendarActivity.java
+++ app/src/main/java/com/example/netonboard/netonboardv2/CalendarActivity.java
@@ -6,24 +6,24 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
 
 public class CalendarActivity extends AppCompatActivity {
 
+    CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("Company Calendar");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        calendarView = (CalendarView)findViewById(R.id.calendar_view);
     }
+
+
 
 }
